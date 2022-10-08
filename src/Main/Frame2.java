e39aa67b5446b43f74848bb40fe28da6294d03a8
M0IZALI <108216563+M0IZALI@users.noreply.github.com>
2022-10-08 20:29:16 +0500

    Update Frame2.java

--- src/Main/Frame2.java
+++ src/Main/Frame2.java
@@ -1,4 +1,5 @@
 package Main;
+
 import javax.swing.*;
 import java.awt.Color;
 public class Frame2 extends JFrame{
@@ -14,38 +15,54 @@ public class Frame2 extends JFrame{
     this.name = name; this.roll_no = roll_no; this.gender = gender; this.batch = batch; this.section = section; this.qualification = qualification; this.address = address; this.country = country;
     new JFrame();
     setTitle("Student Information");
-    setSize(420, 300);
+    setSize(450, 250);
     setLocationRelativeTo(null);
-    this.getContentPane().setBackground(Color.ORANGE);
-    setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+    getContentPane().setBackground(Color.ORANGE);
+    setLayout(null);
     setResizable(false);
 
     jl1 = new JLabel("Name:");
+    jl1.setBounds(60,10,100,50);
     jl2 = new JLabel("Roll.No#:");
+    jl2.setBounds(60,30,100,50);
     jl3 = new JLabel("Gender:");
+    jl3.setBounds(60,50,100,50);
     jl4 = new JLabel("Section:");
+    jl4.setBounds(60,70,100,50);
     jl5 = new JLabel("Batch:");
+    jl5.setBounds(60,90,100,50);
     jl6 = new JLabel("Qualification:");
+    jl6.setBounds(60,110,100,50);
     jl7  = new JLabel("Address:");
+    jl7.setBounds(60,130,100,50);
     jl8 = new JLabel("Country:");
+    jl8.setBounds(60,150,100,50);
     
     //Printing the student data.
     show_Name = new JLabel(name);
     show_Name.setForeground(Color.RED);
+    show_Name.setBounds(150,10,100,50);
     show_Roll_No = new JLabel(roll_no);
     show_Roll_No.setForeground(Color.RED);
+    show_Roll_No.setBounds(150,30,100,50);
     show_Gender = new JLabel(gender);
     show_Gender.setForeground(Color.RED);
+    show_Gender.setBounds(150,50,100,50);
     show_Batch = new JLabel(batch);
     show_Batch.setForeground(Color.RED);
+    show_Batch.setBounds(150,70,100,50);
     show_Section = new JLabel(section);
     show_Section.setForeground(Color.RED);
+    show_Section.setBounds(150,90,100,50);
     show_Qualification = new JLabel(qualification);
     show_Qualification.setForeground(Color.RED);
+    show_Qualification.setBounds(150,110,400,50);
     show_Address = new JLabel(address);
     show_Address.setForeground(Color.RED);
+    show_Address.setBounds(150,130,400,50);
     show_Country = new JLabel(country);
     show_Country.setForeground(Color.RED);
+    show_Country.setBounds(150,150,100,50);
 
     add(jl1); add(show_Name);
     add(jl2); add(show_Roll_No);
